a84230503402e4d5613e0febcdf541c901525fd4
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2025-11-08 14:53:56 -0500

    Added Config files to store the variables as well as Schema Validation

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT ?? 3000;
     await app.listen(port);
     Logger.log(`Application is running on: http://localhost:${port}`);
-    
 }
 bootstrap();
